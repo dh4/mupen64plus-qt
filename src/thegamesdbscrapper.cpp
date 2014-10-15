a3826400a3d8412e89258639a863d088fffc49ac
Dan Hasting <dan@hasting.info>
2014-10-15 12:20:05 -0400

    Simplify things slightly

--- src/thegamesdbscrapper.cpp
+++ src/thegamesdbscrapper.cpp
@@ -42,145 +42,143 @@ TheGamesDBScrapper::TheGamesDBScrapper(QWidget *parent, bool force) : QObject(pa
 
 void TheGamesDBScrapper::downloadGameInfo(QString identifier, QString searchName, QString gameID)
 {
-    if (keepGoing) {
+    if (keepGoing && identifier != "") {
         if (force) parent->setEnabled(false);
 
-        if (identifier != "") {
-            bool updated = false;
+        bool updated = false;
 
-            QString gameCache = getDataLocation() + "/cache/" + identifier.toLower();
-            QDir cache(gameCache);
+        QString gameCache = getDataLocation() + "/cache/" + identifier.toLower();
+        QDir cache(gameCache);
 
-            if (!cache.exists()) {
-                cache.mkpath(gameCache);
-            }
-
-            //Get game XML info from thegamesdb.net
-            QString dataFile = gameCache + "/data.xml";
-            QFile file(dataFile);
-
-            if (!file.exists() || file.size() == 0 || force) {
-                QUrl url;
-
-                //Remove [!], (U), etc. from GoodName for searching
-                searchName.remove(QRegExp("\\W*(\\(|\\[).+(\\)|\\])\\W*"));
-
-                //Few game specific hacks
-                //TODO: Contact thegamesdb.net and see if these can be fixed on their end
-                if (searchName == "Legend of Zelda, The - Majora's Mask")
-                    searchName = "Majora's Mask";
-                else if (searchName == "Legend of Zelda, The - Ocarina of Time - Master Quest")
-                    searchName = "Master Quest";
-                else if (searchName.toLower() == "f-zero x")
-                    gameID = "10836";
-
-                //If user submits gameID, use that
-                if (gameID != "")
-                    url.setUrl("http://thegamesdb.net/api/GetGame.php?id="
-                               + gameID + "&platform=Nintendo 64");
-                else
-                    url.setUrl("http://thegamesdb.net/api/GetGame.php?name="
-                               + searchName + "&platform=Nintendo 64");
-
-                QString dom = getUrlContents(url);
-
-                QDomDocument xml;
-                xml.setContent(dom);
-                QDomNode node = xml.elementsByTagName("Data").at(0).firstChildElement("Game");
-
-                int count = 0, found = 0;
-
-                while(!node.isNull())
-                {
-                    QDomElement element = node.firstChildElement("GameTitle").toElement();
-
-                    if (force) { //from user dialog
-                        QDomElement date = node.firstChildElement("ReleaseDate").toElement();
-
-                        QString check = "Game: " + element.text();
-                        check.remove(QRegExp(QString("[^A-Za-z 0-9 \\.,\\?'""!@#\\$%\\^&\\*\\")
-                                             + "(\\)-_=\\+;:<>\\/\\\\|\\}\\{\\[\\]`~]*"));
-                        if (date.text() != "") check += "\nReleased on: " + date.text();
-                        check += "\n\nDoes this look correct?";
-
-                        int answer = QMessageBox::question(parent, QObject::tr("Game Information Download"),
-                                                           check, QMessageBox::Yes | QMessageBox::No);
-
-                        if (answer == QMessageBox::Yes) {
-                            found = count;
-                            updated = true;
-                            break;
-                        }
-                    } else {
-                        //We only want one game, so search for a perfect match in the GameTitle element.
-                        //Otherwise this will default to 0 (the first game found)
-                        if(element.text() == searchName)
-                            found = count;
-                    }
-
-                    node = node.nextSibling();
-                    count++;
-                }
-
-                if (!force || updated) {
-                    file.open(QIODevice::WriteOnly);
-                    QTextStream stream(&file);
-
-                    QDomNodeList gameList = xml.elementsByTagName("Game");
-                    gameList.at(found).save(stream, QDomNode::EncodingFromDocument);
-
-                    file.close();
-                }
-
-                if (force && !updated) {
-                    QString message;
-
-                    if (count == 0)
-                        message = QObject::tr("No results found.");
-                    else
-                        message = QObject::tr("No more results found.");
-
-                    QMessageBox::information(parent, QObject::tr("Game Information Download"), message);
-                }
-            }
-
-
-            //Get front cover
-            QString boxartURL = "";
-            QString coverFile = gameCache + "/boxart-front.jpg";
-            QFile cover(coverFile);
-
-            if (!cover.exists() || (force && updated)) {
-                file.open(QIODevice::ReadOnly);
-                QString dom = file.readAll();
-                file.close();
-
-                QDomDocument xml;
-                xml.setContent(dom);
-                QDomNode node = xml.elementsByTagName("Game").at(0).firstChildElement("Images").firstChild();
-
-                while(!node.isNull())
-                {
-                    QDomElement element = node.toElement();
-                    if(element.tagName() == "boxart" && element.attribute("side") == "front")
-                        boxartURL = element.attribute("thumb");
-
-                    node = node.nextSibling();
-                }
-
-                if (boxartURL != "") {
-                    QUrl url("http://thegamesdb.net/banners/" + boxartURL);
-
-                    cover.open(QIODevice::WriteOnly);
-                    cover.write(getUrlContents(url));
-                    cover.close();
-                }
-            }
-
-            if (updated)
-                QMessageBox::information(parent, QObject::tr("Game Information Download"),
-                                         QObject::tr("Download Complete!"));
+        if (!cache.exists()) {
+            cache.mkpath(gameCache);
         }
+
+        //Get game XML info from thegamesdb.net
+        QString dataFile = gameCache + "/data.xml";
+        QFile file(dataFile);
+
+        if (!file.exists() || file.size() == 0 || force) {
+            QUrl url;
+
+            //Remove [!], (U), etc. from GoodName for searching
+            searchName.remove(QRegExp("\\W*(\\(|\\[).+(\\)|\\])\\W*"));
+
+            //Few game specific hacks
+            //TODO: Contact thegamesdb.net and see if these can be fixed on their end
+            if (searchName == "Legend of Zelda, The - Majora's Mask")
+                searchName = "Majora's Mask";
+            else if (searchName == "Legend of Zelda, The - Ocarina of Time - Master Quest")
+                searchName = "Master Quest";
+            else if (searchName.toLower() == "f-zero x")
+                gameID = "10836";
+
+            //If user submits gameID, use that
+            if (gameID != "")
+                url.setUrl("http://thegamesdb.net/api/GetGame.php?id="
+                           + gameID + "&platform=Nintendo 64");
+            else
+                url.setUrl("http://thegamesdb.net/api/GetGame.php?name="
+                           + searchName + "&platform=Nintendo 64");
+
+            QString dom = getUrlContents(url);
+
+            QDomDocument xml;
+            xml.setContent(dom);
+            QDomNode node = xml.elementsByTagName("Data").at(0).firstChildElement("Game");
+
+            int count = 0, found = 0;
+
+            while(!node.isNull())
+            {
+                QDomElement element = node.firstChildElement("GameTitle").toElement();
+
+                if (force) { //from user dialog
+                    QDomElement date = node.firstChildElement("ReleaseDate").toElement();
+
+                    QString check = "Game: " + element.text();
+                    check.remove(QRegExp(QString("[^A-Za-z 0-9 \\.,\\?'""!@#\\$%\\^&\\*\\")
+                                         + "(\\)-_=\\+;:<>\\/\\\\|\\}\\{\\[\\]`~]*"));
+                    if (date.text() != "") check += "\nReleased on: " + date.text();
+                    check += "\n\nDoes this look correct?";
+
+                    int answer = QMessageBox::question(parent, QObject::tr("Game Information Download"),
+                                                       check, QMessageBox::Yes | QMessageBox::No);
+
+                    if (answer == QMessageBox::Yes) {
+                        found = count;
+                        updated = true;
+                        break;
+                    }
+                } else {
+                    //We only want one game, so search for a perfect match in the GameTitle element.
+                    //Otherwise this will default to 0 (the first game found)
+                    if(element.text() == searchName)
+                        found = count;
+                }
+
+                node = node.nextSibling();
+                count++;
+            }
+
+            if (!force || updated) {
+                file.open(QIODevice::WriteOnly);
+                QTextStream stream(&file);
+
+                QDomNodeList gameList = xml.elementsByTagName("Game");
+                gameList.at(found).save(stream, QDomNode::EncodingFromDocument);
+
+                file.close();
+            }
+
+            if (force && !updated) {
+                QString message;
+
+                if (count == 0)
+                    message = QObject::tr("No results found.");
+                else
+                    message = QObject::tr("No more results found.");
+
+                QMessageBox::information(parent, QObject::tr("Game Information Download"), message);
+            }
+        }
+
+
+        //Get front cover
+        QString boxartURL = "";
+        QString coverFile = gameCache + "/boxart-front.jpg";
+        QFile cover(coverFile);
+
+        if (!cover.exists() || (force && updated)) {
+            file.open(QIODevice::ReadOnly);
+            QString dom = file.readAll();
+            file.close();
+
+            QDomDocument xml;
+            xml.setContent(dom);
+            QDomNode node = xml.elementsByTagName("Game").at(0).firstChildElement("Images").firstChild();
+
+            while(!node.isNull())
+            {
+                QDomElement element = node.toElement();
+                if(element.tagName() == "boxart" && element.attribute("side") == "front")
+                    boxartURL = element.attribute("thumb");
+
+                node = node.nextSibling();
+            }
+
+            if (boxartURL != "") {
+                QUrl url("http://thegamesdb.net/banners/" + boxartURL);
+
+                cover.open(QIODevice::WriteOnly);
+                cover.write(getUrlContents(url));
+                cover.close();
+            }
+        }
+
+        if (updated)
+            QMessageBox::information(parent, QObject::tr("Game Information Download"),
+                                     QObject::tr("Download Complete!"));
 
         if (force) parent->setEnabled(true);
     }
