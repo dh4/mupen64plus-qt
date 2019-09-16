cfecf7ed24921eed4cdde59a45087e630f5b31f8
Dan Hasting <commits@hasting.email>
2019-09-15 21:30:24 -0400

    Update MXE configuration

--- build-scripts/platform/windows.sh
+++ build-scripts/platform/windows.sh
@@ -17,7 +17,7 @@ case "$1" in
                 gettext git g++ gperf intltool libffi-dev libtool \
                 libltdl-dev libssl-dev libxml-parser-perl make openssl \
                 p7zip-full patch perl pkg-config python ruby scons sed \
-                unzip wget xz-utils
+                unzip wget xz-utils libtool-bin lzip libgdk-pixbuf2.0-dev
             git clone https://github.com/mxe/mxe.git
             cd mxe
             make qtbase
