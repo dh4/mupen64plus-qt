093f55c0f80765d6001e7ecf85ce2af4e7243537
Dan Hasting <commits@hasting.email>
2024-11-10 18:22:23 -0500

    Fix bug with revision script

--- build-scripts/revision.sh
+++ build-scripts/revision.sh
@@ -4,7 +4,7 @@ git show-ref --tags | grep "`git log -n 1 --pretty='%H'`" > /dev/null 2>&1
 
 if [[ $? == 1 ]]; then
     REVISION=$(git log -n 1 --pretty='%h' 2> /dev/null)
-    [[ $REVISION ]] && sed -i -e "s/\([0-9.]*\)/\1~git:$REVISION/g" VERSION
+    [[ $REVISION && `grep "$REVISION" VERSION | wc -l` -eq 0 ]] && sed -i -e "s/\([0-9.]*\)/\1~git:$REVISION/g" VERSION
 fi
 
 cat VERSION
